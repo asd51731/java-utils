dcac5fb631a7c64f6be6c425f108efa02b40f4db
zhangxiaxu <zhangxiaxu@soulapp.cn>
2020-01-07 19:41:54 +0800

    init

---
+++ src/test/java/Test1.java
@@ -0,0 +1,12 @@
+/**
+ * @Description
+ * @Author asd51731
+ * @Date 2020-01-07
+ * @Ide IntelliJ IDEA
+ */
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
